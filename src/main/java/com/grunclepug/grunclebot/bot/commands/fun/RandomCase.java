aee238162dbcc3154dd0622f20c2cfd06aa45a48
Chad Humphries <chadrox999@gmail.com>
2020-10-07 14:22:54 -0700

    Fix RandomCase Bug allowing user to bypass permissions

--- src/main/java/com/grunclepug/grunclebot/bot/commands/fun/RandomCase.java
+++ src/main/java/com/grunclepug/grunclebot/bot/commands/fun/RandomCase.java
@@ -2,6 +2,7 @@ package com.grunclepug.grunclebot.bot.commands.fun;
 
 import com.grunclepug.grunclebot.bot.core.Config;
 import com.grunclepug.grunclebot.bot.util.log.BotLog;
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
@@ -15,19 +16,31 @@ public class RandomCase extends ListenerAdapter
 
         if(args[0].equalsIgnoreCase(Config.getPrefix() + "randomcase"))
         {
-            Random rand = new Random();
-            String content = event.getMessage().getContentRaw().substring(args[0].length()).toLowerCase().trim();
-            char[] chars = content.toCharArray();
+            if(args[1].startsWith(Config.getPrefix())) {
+                Random rand = new Random();
+                String content = event.getMessage().getContentRaw().substring(args[0].length()).toLowerCase().trim();
+                char[] chars = content.toCharArray();
 
-            for(int i = 0; i < content.length(); i++)
-            {
-                if(rand.nextBoolean())
+                for(int i = 0; i < content.length(); i++)
                 {
-                    chars[i] = Character.toUpperCase(chars[i]);
+                    if(rand.nextBoolean())
+                    {
+                        chars[i] = Character.toUpperCase(chars[i]);
+                    }
                 }
+                event.getChannel().sendTyping().queue();
+                event.getChannel().sendMessage(String.copyValueOf(chars)).queue();
+            } else {
+                // Lack in Perms
+                EmbedBuilder builder = new EmbedBuilder();
+                builder.setTitle("\uD83D\uDED1 Nice try")
+                        .setDescription("You cannot issue a command inside of this command!")
+                        .setColor(0xff3923);
+
+                event.getChannel().sendTyping().queue();
+                event.getChannel().sendMessage(builder.build()).queue();
+                builder.clear();
             }
-            event.getChannel().sendTyping().queue();
-            event.getChannel().sendMessage(String.copyValueOf(chars)).queue();
 
             BotLog.log(event);
         }
