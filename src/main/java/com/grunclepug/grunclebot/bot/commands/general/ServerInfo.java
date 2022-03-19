bf5c965f990db6aa2ee1b71d5b81990d58d57bde
unknown <chadrox999@gmail.com>
2022-03-19 16:48:37 -0700

    Update UserInfo and ServerInfo to accept ID's

--- src/main/java/com/grunclepug/grunclebot/bot/commands/general/ServerInfo.java
+++ src/main/java/com/grunclepug/grunclebot/bot/commands/general/ServerInfo.java
@@ -1,8 +1,10 @@
 package com.grunclepug.grunclebot.bot.commands.general;
 
 import com.grunclepug.grunclebot.bot.core.Config;
+import com.grunclepug.grunclebot.bot.core.Driver;
 import com.grunclepug.grunclebot.bot.util.log.BotLog;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -20,16 +22,23 @@ public class ServerInfo extends ListenerAdapter {
 
         if(args[0].equalsIgnoreCase(Config.getPrefix() + "serverinfo") || args[0].equalsIgnoreCase(Config.getPrefix() + "guildinfo")) {
             Date date = new Date();
+            Guild guild = null;
+
+            if(args.length > 1) {
+                guild = Driver.jda.getGuildById(args[1]);
+            } else {
+                guild = event.getGuild();
+            }
 
             EmbedBuilder builder = new EmbedBuilder();
-            builder.setTitle("Server: " + event.getGuild().getName())
-                    .setThumbnail(event.getGuild().getIconUrl())
-                    .addField("・Owner", event.getGuild().getOwner().getAsMention(), false)
-                    .addField("・Date Created", event.getGuild().getTimeCreated().format(DateTimeFormatter.ofPattern("MMM dd, yyyy 'at' HH:mm")), false)
-                    .addField("・Members", "" + event.getGuild().getMembers().size(), false)
-                    .addField("・Roles", "" + event.getGuild().getRoles().size(), false)
-                    .addField("・Text Channels", "" + event.getGuild().getTextChannels().size(), false)
-                    .addField("・Voice Channels", "" + event.getGuild().getVoiceChannels().size(), false)
+            builder.setTitle("Server: " + guild.getName())
+                    .setThumbnail(guild.getIconUrl())
+                    .addField("・Owner", guild.getOwner().getAsMention(), false)
+                    .addField("・Date Created", guild.getTimeCreated().format(DateTimeFormatter.ofPattern("MMM dd, yyyy 'at' HH:mm")), false)
+                    .addField("・Members", "" + guild.getMembers().size(), false)
+                    .addField("・Roles", "" + guild.getRoles().size(), false)
+                    .addField("・Text Channels", "" + guild.getTextChannels().size(), false)
+                    .addField("・Voice Channels", "" + guild.getVoiceChannels().size(), false)
                     .setFooter(Config.DATE_FORMAT.format(date), "https://i.imgur.com/WQSW5lV.png")
                     .setColor(0x00FF00);
 
--- src/main/java/com/grunclepug/grunclebot/bot/commands/general/UserInfo.java
+++ src/main/java/com/grunclepug/grunclebot/bot/commands/general/UserInfo.java
@@ -1,9 +1,11 @@
 package com.grunclepug.grunclebot.bot.commands.general;
 
 import com.grunclepug.grunclebot.bot.core.Config;
+import com.grunclepug.grunclebot.bot.core.Driver;
 import com.grunclepug.grunclebot.bot.util.log.BotLog;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -20,53 +22,74 @@ public class UserInfo extends ListenerAdapter {
         String[] args = event.getMessage().getContentRaw().split("\\s+");
 
         if(args[0].equalsIgnoreCase(Config.getPrefix() + "userinfo")) {
-            Member member;
+            Member member = null;
+            User user = null;
             if(args.length > 1) {
-                member = event.getMessage().getMentionedMembers().get(0);
+                if(event.getMessage().getMentionedMembers().size() > 0) {
+                    member = event.getMessage().getMentionedMembers().get(0);
+                } else {
+                    user = Driver.jda.getUserById(args[1]);
+                }
             } else {
                 member = event.getMember();
             }
 
-            Date date = new Date();
-            String nickname = member.getNickname();
-            String status = member.getOnlineStatus().name();
+            if(user != null) {
+                Date date = new Date();
 
-            if(nickname == null) {
-                nickname = "n/a";
+                EmbedBuilder builder = new EmbedBuilder();
+                builder.setThumbnail(member.getUser().getAvatarUrl())
+                        .setTitle("User Info:")
+                        .addField("・User", user.getAsTag(), false)
+                        .addField("・ID", user.getId(), false)
+                        .addField("・Account Created", user.getTimeCreated().format(DateTimeFormatter.ofPattern("MMM dd, yyyy 'at' HH:mm")), false)
+                        .setFooter(Config.DATE_FORMAT.format(date), "https://i.imgur.com/WQSW5lV.png")
+                        .setColor(0x00FF00);
+
+                event.getChannel().sendTyping().queue();
+                event.getChannel().sendMessageEmbeds(builder.build()).queue();
+                builder.clear();
+            } else {
+                Date date = new Date();
+                String nickname = member.getNickname();
+                String status = member.getOnlineStatus().name();
+
+                if(nickname == null) {
+                    nickname = "n/a";
+                }
+
+                switch(status) {
+                    case "OFFLINE":
+                        status = "Offline";
+                        break;
+                    case "ONLINE":
+                        status = "Online";
+                        break;
+                    case "IDLE":
+                        status = "Idle";
+                        break;
+                    case "DO_NOT_DISTURB":
+                        status = "Do Not Disturb";
+                        break;
+                }
+
+                EmbedBuilder builder = new EmbedBuilder();
+                builder.setThumbnail(member.getUser().getAvatarUrl())
+                        .setTitle("User Info:")
+                        .addField("・User", member.getUser().getAsTag(), false)
+                        .addField("・Nickname", "" + nickname, false)
+                        .addField("・ID", member.getUser().getId(), false)
+                        .addField("・Status", status, false)
+                        .addField("・Account Created", member.getUser().getTimeCreated().format(DateTimeFormatter.ofPattern("MMM dd, yyyy 'at' HH:mm")), false)
+                        .addField("・Server Join Date", member.getTimeJoined().format(DateTimeFormatter.ofPattern("MMM dd, yyyy 'at' HH:mm")), false)
+                        .addField("・Roles", "" + member.getRoles().size(), false)
+                        .setFooter(Config.DATE_FORMAT.format(date), "https://i.imgur.com/WQSW5lV.png")
+                        .setColor(0x00FF00);
+
+                event.getChannel().sendTyping().queue();
+                event.getChannel().sendMessageEmbeds(builder.build()).queue();
+                builder.clear();
             }
-
-            switch(status) {
-                case "OFFLINE":
-                    status = "Offline";
-                    break;
-                case "ONLINE":
-                    status = "Online";
-                    break;
-                case "IDLE":
-                    status = "Idle";
-                    break;
-                case "DO_NOT_DISTURB":
-                    status = "Do Not Disturb";
-                    break;
-            }
-
-            EmbedBuilder builder = new EmbedBuilder();
-            builder.setThumbnail(member.getUser().getAvatarUrl())
-                    .setTitle("User Info:")
-                    .addField("・User", member.getUser().getAsTag(), false)
-                    .addField("・Nickname", "" + nickname, false)
-                    .addField("・ID", member.getUser().getId(), false)
-                    .addField("・Status", status, false)
-                    .addField("・Account Created", member.getUser().getTimeCreated().format(DateTimeFormatter.ofPattern("MMM dd, yyyy 'at' HH:mm")), false)
-                    .addField("・Server Join Date", member.getTimeJoined().format(DateTimeFormatter.ofPattern("MMM dd, yyyy 'at' HH:mm")), false)
-                    .addField("・Roles", "" + member.getRoles().size(), false)
-                    .setFooter(Config.DATE_FORMAT.format(date), "https://i.imgur.com/WQSW5lV.png")
-                    .setColor(0x00FF00);
-
-            event.getChannel().sendTyping().queue();
-            event.getChannel().sendMessageEmbeds(builder.build()).queue();
-            builder.clear();
-
             BotLog.log(event);
         }
     }
